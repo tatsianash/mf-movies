f78ca2bdb1275c961280b553e02cd640bbc7a91d
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-30 22:01:57 -0700

    Add mocies and playlist  content

---
+++ packages/playlist-content/PlaylistContent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Title, Grid } from '@mantine/core';
+
+import { useStore } from 'store';
+import MovieCard from 'card';
+
+export default function PlaylistContent() {
+  const { movies } = useStore();
+  return (
+    <>
+      <Box>
+        <Title>Viewing List</Title>
+      </Box>
+      <Grid
+        mt={20}
+        sx={{
+          gap: '1rem',
+        }}>
+        {movies.map((movie) => (
+          <MovieCard {...movie} key={movie.title} />
+        ))}
+      </Grid>
+    </>
+  );
+}
