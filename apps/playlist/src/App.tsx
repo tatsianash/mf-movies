ffa38022592162b6f7cb28ec0349314a7a90755b
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-31 10:38:08 -0700

    Runtime sharing with fallback to buildtime

--- apps/playlist/src/App.tsx
+++ apps/playlist/src/App.tsx
@@ -1,9 +1,39 @@
 import { AppShell } from 'ui';
 import React from 'react';
 import { PlaylistContent } from 'playlist-content';
+import { MoviesContent } from 'movies-content';
 import './App.css';
+
 // @ts-ignore
-import MoviesContent from 'movies/Movies';
+const MoviesContentRuntime = React.lazy(() => import('movies/Movies'));
+
+class ErrorBoundary extends React.Component<
+  { children: React.ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props: { children: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch() {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <MoviesContent />;
+    }
+    return this.props.children;
+  }
+}
 
 function App() {
   return (
@@ -19,7 +49,11 @@ function App() {
         routes={[
           {
             path: '/',
-            element: MoviesContent,
+            element: () => (
+              <ErrorBoundary>
+                <MoviesContentRuntime />
+              </ErrorBoundary>
+            ),
           },
           {
             path: '/playlist',
