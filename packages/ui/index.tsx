0160b659e611274a9b13c0890504e29f3703a89b
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-30 17:43:44 -0700

    Configure git ignore

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,16 +1,36 @@
 import * as React from 'react';
-import { AppShell as MantineAppShell, Header, Title } from '@mantine/core';
+import {
+  AppShell as MantineAppShell,
+  Header,
+  Title,
+  MantineProvider,
+} from '@mantine/core';
 
 interface AppShellProps {
   title: string;
+  colorScheme?: 'dark' | 'light';
 }
-export const AppShell = ({ title }: AppShellProps) => (
-  <MantineAppShell
-    header={
-      <Header height={60} p="xs" sx={{ display: 'flex' }}>
-        <Title>{title}</Title>
-      </Header>
-    }>
-    <div>hello</div>
-  </MantineAppShell>
+export const AppShell = ({ title, colorScheme }: AppShellProps) => (
+  <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+    <MantineAppShell
+      padding="sm"
+      header={
+        <Header
+          height={60}
+          p="xs"
+          sx={{ display: 'flex' }}
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}>
+          <Title>{title}</Title>
+        </Header>
+      }>
+      <div>hello ther</div>
+    </MantineAppShell>
+  </MantineProvider>
 );
