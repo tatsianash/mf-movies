a1c2b709e241cdecd47ca6f6b8e0d4b79e80f907
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-30 19:39:12 -0700

    Add Routes

--- apps/movies/src/App.tsx
+++ apps/movies/src/App.tsx
@@ -8,7 +8,24 @@ function App() {
   return (
     <div>
       <h1>Movies</h1>
-      <AppShell title="Movies app" />
+      <AppShell
+        title="Movies app"
+        colorScheme="light"
+        navLinks={[
+          { label: 'Home', path: '/' },
+          { label: 'Playlist', path: '/playlist' },
+        ]}
+        routes={[
+          {
+            path: '/',
+            element: () => <div>Home</div>,
+          },
+          {
+            path: '/playlist',
+            element: () => <div>Playlist</div>,
+          },
+        ]}
+      />
       {/* <Button /> */}
     </div>
   );
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -4,33 +4,98 @@ import {
   Header,
   Title,
   MantineProvider,
+  Navbar,
+  UnstyledButton,
+  Group,
+  Text,
 } from '@mantine/core';
+import { BrowserRouter, Outlet, Link, Route, Routes } from 'react-router-dom';
 
+export type RouteType = {
+  element: React.FunctionComponent;
+  path: string;
+};
 interface AppShellProps {
   title: string;
   colorScheme?: 'dark' | 'light';
+  routes: RouteType[];
+  navLinks: NavLink[];
 }
-export const AppShell = ({ title, colorScheme }: AppShellProps) => (
-  <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
-    <MantineAppShell
-      padding="sm"
-      header={
-        <Header
-          height={60}
-          p="xs"
-          sx={{ display: 'flex' }}
-          styles={(theme) => ({
-            main: {
-              backgroundColor:
-                theme.colorScheme === 'dark'
-                  ? theme.colors.dark[8]
-                  : theme.colors.gray[0],
-            },
-          })}>
-          <Title>{title}</Title>
-        </Header>
-      }>
-      <div>hello ther</div>
-    </MantineAppShell>
-  </MantineProvider>
+
+export type NavLink = {
+  label: string;
+  path: string;
+};
+function MainLink({ label, path }: NavLink) {
+  return (
+    <Link to={path}>
+      <UnstyledButton
+        sx={(theme) => ({
+          display: 'block',
+          wigth: '100%',
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color:
+            theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+          '&:hover': {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+          },
+        })}>
+        <Group>
+          <Text size="sm">{label}</Text>
+        </Group>
+      </UnstyledButton>
+    </Link>
+  );
+}
+
+export const AppShell = ({
+  title,
+  colorScheme,
+  routes,
+  navLinks,
+}: AppShellProps) => (
+  <BrowserRouter>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+      <MantineAppShell
+        padding="sm"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            {navLinks.map((link) => (
+              <MainLink {...link} key={link.path} />
+            ))}
+          </Navbar>
+        }
+        header={
+          <Header
+            height={60}
+            p="xs"
+            sx={{ display: 'flex' }}
+            styles={(theme) => ({
+              main: {
+                backgroundColor:
+                  theme.colorScheme === 'dark'
+                    ? theme.colors.dark[8]
+                    : theme.colors.gray[0],
+              },
+            })}>
+            <Title>{title}</Title>
+          </Header>
+        }>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.element />}
+            />
+          ))}
+        </Routes>
+        <Outlet />
+      </MantineAppShell>
+    </MantineProvider>
+  </BrowserRouter>
 );
